b5627b4fad26a325f192c3cd517026c5544f2e6e
DESKTOP-7QMDHSD\TUANPH-LAPTOP <tuan.phamhoang.2506@gmail.com>
2017-02-12 12:38:57 +0700

    Aplly gemtelella  theme

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+const securityModule_1 = require("./securityModule");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(securityModule_1.SecurityModule);
+//# sourceMappingURL=main.js.map
