b903cbe1ae16d32dac49f721eb711a208377cf67
DESKTOP-7QMDHSD\TUANPH-LAPTOP <tuan.phamhoang.2506@gmail.com>
2017-03-27 20:09:19 +0700

    no message

--- client/src/httpConnector.ts
+++ client/src/httpConnector.ts
@@ -5,7 +5,6 @@ import appHelper from "./appHelper";
 import appConfig from "./appConfig";
 import "rxjs/add/operator/map";
 export class HttpConnector implements IConnector {
-
     private rootUrl = appConfig.rootApiUrl;
     public get(url: string): Promise {
         let http: Http = appHelper.injector.get(Http);
@@ -24,22 +23,6 @@ export class HttpConnector implements IConnector {
     }
     private handleResponse(response: Response) {
         return response.json().data;
-    }
-
-    public post(url: string, item: any): Promise {
-        return null;
-        //     let promise = PromiseFactory.create();
-        //     url = this.rootUrl + url;
-        //     this.http.post(url, item)
-        //         .map(this.handleResponse)
-        //         .subscribe(
-        //         (data: any) => {
-        //             promise.resolve(data)
-        //         },
-        //         (error: any) => {
-        //             promise.reject(error)
-        //         });
-        //     return promise;
     }
 
 }
--- client/src/iConnector.ts
+++ client/src/iConnector.ts
@@ -1,5 +1,5 @@
 import { Promise } from "./promise";
 export interface IConnector {
     get(url: string): Promise;
-    post(url: string, item: any): Promise;
+  
 }
